a6a94686d1842aeb96c6db4a5d9c542511e8ac3c
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2020-02-12 22:31:05 +0700

    Add baseline

---
+++ MTH058/baseline/download_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clean all db
+rm *-ubyte.gz
+rm *-ubyte
+
+# Download all db
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+# Extract all db
+gzip -d *-ubyte.gz
+
