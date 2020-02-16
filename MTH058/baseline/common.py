d43842c4940ca5b837d72b8ed47408dc6b290a1f
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2020-02-16 13:27:13 +0700

    Update baseline: training

--- MTH058/baseline/common.py
+++ MTH058/baseline/common.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
+import os
+
 import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+# Metadata
+input_size = 28 * 28
+output_size = 10
+input_layer = input_size
+layer_1 = 500
+layer_2 = 150
+output_layer = output_size
+
+def create_dir_if_not_exists(path):
+  if not os.path.isdir(path):
+    os.makedirs(path)
 
 def read_bin_file(name):
   with open(name, "rb") as f:
@@ -10,13 +28,34 @@ def read_images(prefix):
   name = prefix + "-images-idx3-ubyte"
   data = read_bin_file(name)
   num = int.from_bytes(data[4:8], "big")
-  images = np.frombuffer(data, dtype=np.uint8, offset=16).reshape([num, 28 * 28])
+  images = np.frombuffer(data, dtype=np.uint8, offset=16).reshape([num, input_size]).astype("float32")
   return images
 
 def read_labels(prefix):
   name = prefix + "-labels-idx1-ubyte"
   data = read_bin_file(name)
   num = int.from_bytes(data[4:8], "big")
-  labels = np.frombuffer(data, dtype=np.uint8, offset=8)
+  labels = np.frombuffer(data, dtype=np.uint8, offset=8).reshape([num, 1]).astype("float32")
   return labels
+
+def category2binary(x):
+  temp = [None] * output_size
+  for i in range(output_size):
+    temp[i] = np.zeros(output_size)
+    temp[i][i] = 1
+  return np.apply_along_axis(lambda a: temp[int(a[0])], 1, x).astype("float32")
+
+def binary2category(x):
+  return np.apply_along_axis(lambda a: a @ np.arange(10), 1, x).reshape([x.shape[0], 1]).astype("float32")
+
+def create_checkpoint_callback(path, period):
+  return ModelCheckpoint(filepath=path, verbose=1, save_weights_only=True, period=period)
+
+def create_model():
+  model = Sequential()
+  model.add(Dense(layer_1, activation="sigmoid", input_shape=(input_layer,)))
+  model.add(Dense(layer_2, activation="sigmoid"))
+  model.add(Dense(output_layer, activation="softmax"))
+  model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["categorical_accuracy"])
+  return model
 
--- MTH058/baseline/train.py
+++ MTH058/baseline/train.py
@@ -2,8 +2,27 @@
 
 import common
 
+# Metadata
+batch_size = 128
+epochs = 50
+checkpoint_dir = "training"
+checkpoint_format = "weights.{epoch:04d}-{val_loss:.2f}.h5"
+period = 5
+
+# Create checkpoint directory if not exists
+common.create_dir_if_not_exists(checkpoint_dir)
+
+# Load data
 prefix = "train"
 images = common.read_images(prefix)
-labels = common.read_labels(prefix)
+ori_labels = common.read_labels(prefix)
+new_labels = common.category2binary(ori_labels)
 
-import tensorflow as tf
+# Create model
+model = common.create_model()
+
+# Train model
+model.fit(images, new_labels, batch_size=batch_size, epochs=epochs,
+          callbacks=[common.create_checkpoint_callback(checkpoint_dir+"/"+checkpoint_format, period)],
+          validation_split=0.3, shuffle=True)
+
