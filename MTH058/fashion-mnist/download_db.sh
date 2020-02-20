077106d4b554d75329f943730744a6690399e7ae
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2020-02-21 00:00:49 +0700

    Add fashion-mnist

---
+++ MTH058/fashion-mnist/download_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clean all db
+rm *-ubyte.gz
+rm *-ubyte
+
+# Download all db
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz
+
+# Extract all db
+gzip -d *-ubyte.gz
+
