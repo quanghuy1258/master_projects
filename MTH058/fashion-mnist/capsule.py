2de78c83090fd6c307a7c252f927c11ec2f8d393
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2020-02-26 13:05:35 +0700

    Update code: capsule

--- MTH058/fashion-mnist/capsule.py
+++ MTH058/fashion-mnist/capsule.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 
-from keras import backend as K
+from tensorflow.keras import backend as K
+from tensorflow.keras import activations
 from tensorflow.keras.layers import Layer
 
 # Reference: http://keras.io/examples/cifar10_cnn_capsule/
+
+def squash(x):
+  x_squared_norm = K.sum(K.square(x)) + K.epsilon()
+  scale = K.sqrt(x_squared_norm) / (1.0 + x_squared_norm)
+  return scale * x
+
 class Capsule(Layer):
-  def __init__(self, num_capsule, dim_capsule, **kwargs):
+  def __init__(self, num_capsule, dim_capsule, routings=3,  activation="squash",**kwargs):
     self.num_capsule = num_capsule
     self.dim_capsule = dim_capsule
+    self.routings = routings
+    if activation == "squash":
+      self.activation = squash
+    else:
+      self.activation = activations.get(activation)
     super(Capsule, self).__init__(**kwargs)
 
   def build(self, input_shape):
