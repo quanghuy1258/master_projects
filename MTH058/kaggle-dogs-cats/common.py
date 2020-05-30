910ceff0aa0f542ce4058361460a04848bc7fec9
ptpuyen1511 <ptpuyen1511@gmail.com>
2020-05-30 22:57:32 +0700

    add cnn_model

--- MTH058/kaggle-dogs-cats/common.py
+++ MTH058/kaggle-dogs-cats/common.py
@@ -2,8 +2,8 @@ import os
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import RMSprop, SGD
 
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 
@@ -14,10 +14,10 @@ def create_dir_if_not_exists(path):
         os.makedirs(path)
 
 def create_checkpoint_callback(path, period):
-  return ModelCheckpoint(filepath=path, verbose=1, save_weights_only=True, period=period)
+    return ModelCheckpoint(filepath=path, verbose=1, save_weights_only=True, period=period)
 
 def create_CSVLogger_callback(training_dir):
-  return CSVLogger("{}/{}".format(training_dir, "log.csv"))
+    return CSVLogger("{}/{}".format(training_dir, "log.csv"))
 
 def create_baseline_model():
     model = Sequential()
@@ -30,6 +30,19 @@ def create_baseline_model():
     model.compile(loss='categorical_crossentropy',
                   optimizer=RMSprop(),
                   metrics=['accuracy'])
+    return model
+
+def create_cnn_model():
+    model = Sequential()
+    
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(64, 64, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 
